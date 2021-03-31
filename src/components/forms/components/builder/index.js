312def582925c0969a5805a9f0e6fd0fc9c86a8d
Emil Petrov <emil.petrov95@gmail.com>
2021-03-31 15:26:22 +0300

    add alt attribute to theme preview

--- src/components/forms/components/builder/index.js
+++ src/components/forms/components/builder/index.js
@@ -6,7 +6,7 @@ const formBuilder = ({ theme, activeMenu, size }) => (
     <div className="builder-wrapper">
         <div className={cn("form-preview flex middle center", size)}>
             {activeMenu === 'theme' && <ThemeSelector />}
-            {theme && <img src={theme.preview} />}
+            {theme && <img src={theme.preview} alt="Theme Preview" />}
         </div>
     </div>
 )
