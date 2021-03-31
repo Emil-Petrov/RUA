0c093e8dad63c3fd17897280b6d0b57e4efd3a21
Emil Petrov <emil.petrov95@gmail.com>
2021-03-31 11:46:32 +0300

    context provider

---
+++ src/components/forms/context/index.js
@@ -0,0 +1,26 @@
+import React, { createContext, useReducer } from "react"
+import appReducer, { initialState, actions as appActions } from './reducer';
+
+export const FormContext = createContext();
+
+const FormContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(appReducer, initialState);
+
+    const actions = {
+        triggerMenu: (menu) => {
+            dispatch({ type: appActions.ACTIVATE_MENU, payload: { menu } });
+        },
+        changeSize: (size) => {
+            dispatch({ type: appActions.CHANGE_SIZE, payload: { size } })
+        },
+    }
+
+    return (
+        <FormContext.Provider value={{ state, actions }}>
+            {children}
+        </FormContext.Provider>
+    )
+}
+
+
+export default FormContextProvider;
